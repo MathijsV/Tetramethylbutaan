ef55ff3128a21736551e0b771dcc960d7776e247
Mathijs <mathijsvos@live.nl>
2012-02-26 18:58:52 +0100

    Commit

--- src/tetramethylbutaan/Main.java
+++ src/tetramethylbutaan/Main.java
@@ -9,7 +9,7 @@ public class Main {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		GabrielGraph gabrielGraph = (GabrielGraph) new TrainingSetReader("mnist_train.txt").getData();
+		GabrielGraph gabrielGraph = (GabrielGraph) new TrainingSetReader("trainSets/train_set_001_n1000_err0.txt").getData();
 		System.out.println("Creating edges...");
 		gabrielGraph.createEdges();
 		System.out.println("Editing graph...");
@@ -17,12 +17,12 @@ public class Main {
 		System.out.println("Condensing graph...");
 		gabrielGraph.condense();
 		
-		System.out.println("Collecting results...");
-		Iterator<Integer> result = new TestSetReader("mnist_train.txt", gabrielGraph).getResults().iterator();
+		//System.out.println("Collecting results...");
+		//Iterator<Integer> result = new TestSetReader("mnist_train.txt", gabrielGraph).getResults().iterator();
 		/*while(result.hasNext())
 			System.out.println(result.next());*/
 		//new TestSetReader("trainSets/train_set_001_n1000_err0.txt", gabrielGraph);*/
-		//new GabrielVisualiser(gabrielGraph);
+		new GabrielVisualiser(gabrielGraph);
 		/*try {
 			new AutoTester(100);
 		} catch (IOException e) {
